2dc0c1174e1e8be142992d353e269e5aa14cfe13
Alon <downtown2u@gmail.com>
2022-05-27 15:01:06 +0300

    ✨ Added dialog for game mode selection

--- app/src/main/java/com/example/modernbackgammon/MainActivity.java
+++ app/src/main/java/com/example/modernbackgammon/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.modernbackgammon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.modernbackgammon.general.GameStateStorage;
 
@@ -33,15 +36,36 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
-    public void newGame(View view) {
-        GameStateStorage.resetGameBoardState();
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
-    }
 
     public void resumeGame(View view) {
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
+    }
+
+    public void newGame(View view) {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.new_game_dialog);
+        dialog.setCancelable(true);
+        dialog.show();
+
+        Button btn = dialog.findViewById(R.id.pvp_button);
+        btn.setOnClickListener(v -> {
+            GameStateStorage.resetGameBoardState();
+            Intent intent = new Intent(this, GameActivity.class);
+            dialog.dismiss();
+            startActivity(intent);
+        });
+
+
+        btn = dialog.findViewById(R.id.pvai_button);
+        btn.setOnClickListener(v -> {
+            GameStateStorage.resetGameBoardState();
+            Intent intent = new Intent(this, GameActivity.class);
+            intent.putExtra("pvai", true);
+            Toast.makeText(this, "AI not yet implemented. Proceeding with local PvP.", Toast.LENGTH_SHORT).show();
+            dialog.dismiss();
+            startActivity(intent);
+        });
     }
 
 }
